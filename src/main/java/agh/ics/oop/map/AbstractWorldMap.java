86cabaa4b0af27c565fd3469e430cbea7cf4bedb
Pejdzor <mikolajpajor@student.agh.edu.pl>
2022-12-22 11:28:17 +0100

    Map Prototype

---
+++ src/main/java/agh/ics/oop/map/AbstractWorldMap.java
@@ -0,0 +1,35 @@
+package agh.ics.oop.map;
+
+import agh.ics.oop.Animal;
+import agh.ics.oop.Vector2d;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AbstractWorldMap {
+    int width,height;
+    GrassGenerator grassField;
+    Map<Vector2d,Animal> animals = new HashMap<>();
+
+    public void place(Animal animal,Vector2d position){
+        if (!isOccupied(position)){
+            this.animals.put(position,animal);
+        }
+    }
+    public boolean isOccupied(Vector2d position){
+        return animals.containsKey(position);
+    }
+    public Animal objectAt(Vector2d position){
+        if (this.animals.containsKey(position)){
+            return this.animals.get(position);
+        }
+        return null;
+    }
+
+    public Vector2d getLowerLeft(){
+        return new Vector2d(0,0);
+    }
+    public Vector2d getUpperRight(){
+        return new Vector2d(this.width,this.height);
+    }
+}
---
+++ src/main/java/agh/ics/oop/map/HellPortal.java
@@ -0,0 +1,9 @@
+package agh.ics.oop.map;
+
+public class HellPortal extends AbstractWorldMap {
+    public HellPortal(int width, int height, GrassGenerator grassField){
+        this.height=height;
+        this.width=width;
+        this.grassField=grassField;
+    }
+}
